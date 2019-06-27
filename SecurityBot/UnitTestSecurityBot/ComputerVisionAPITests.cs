2c8c73069b43aa4437906e8bc1a9fa38135f4532
Rui Gomes <GomesRui@users.noreply.github.com>
2019-06-27 17:51:43 +0100

    add

--- SecurityBot/UnitTestSecurityBot/ComputerVisionAPITests.cs
+++ SecurityBot/UnitTestSecurityBot/ComputerVisionAPITests.cs
@@ -10,33 +10,45 @@ namespace SecurityBot_UnitTests
         private const string subscriptionComputerVision = "fbd6957c05bd45f280165617c9d96c9b";        
 
         [TestMethod]
-        public void CanImageBeValidated_ImageWithPhone_ReturnsFalse() //Method under test _ Scenario tested _ Result
+        public void CanImageBeValidated_ImageWithPhone_ReturnsTrue() //Method under test _ Scenario tested _ Result
         {
             // Arrange
             const string localImagePath = @"C:\Users\v-ruigom\Desktop\test.jpg";
             var computervision = new ComputerVisionAPI(subscriptionComputerVision);
 
             //Act
-            computervision.ApiCall("analyze", localImagePath); //Analyze image
-            var result = computervision.validateImage(); //Validate image
+            var result = computervision.ApiCall("analyze", localImagePath); //Analyze image
 
             //Assert
-            Assert.IsFalse(result);
+            Assert.IsTrue(result.Contains("phone"));
         }
 
         [TestMethod]
-        public void CanImageBeValidated_ImageWithoutPhone_ReturnsTrue() //Method under test _ Scenario tested _ Result
+        public void CanImageBeValidated_ImageWithoutPhone_ReturnsFalse() //Method under test _ Scenario tested _ Result
         {
             // Arrange
             const string localImagePath = @"C:\Users\v-ruigom\Desktop\test3.jpg";
             var computervision = new ComputerVisionAPI(subscriptionComputerVision);
 
             //Act
-            computervision.ApiCall("analyze", localImagePath); //Analyze image
-            var result = computervision.validateImage(); //Validate image
+            var result = computervision.ApiCall("analyze", localImagePath); //Analyze image
 
             //Assert
-            Assert.IsTrue(result);
+            Assert.IsFalse(result.Contains("phone"));
+        }
+
+        [TestMethod]
+        public void CanImageBeValidated_NoImage_ReturnsTrue() //Method under test _ Scenario tested _ Result
+        {
+            // Arrange
+            const string localImagePath = @"C:\Users\v-ruigom\Desktop\tes.jpg";
+            var computervision = new ComputerVisionAPI(subscriptionComputerVision);
+
+            //Act
+            var result = computervision.ApiCall("analyze", localImagePath); //Analyze image
+
+            //Assert
+            Assert.IsTrue(result.Contains(""));
         }
     }
 }
